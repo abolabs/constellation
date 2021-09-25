412e871f3744e03f1a0e4b4ac61990210c483ae0
Alexandre Bordin <bordin.alexandre@gmail.com>
2021-09-25 19:04:30 +0200

    refactor(Datatable): pour Environnement
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- app/DataTables/EnvironnementDataTable.php
+++ app/DataTables/EnvironnementDataTable.php
@@ -6,7 +6,7 @@ use App\Models\Environnement;
 use Yajra\DataTables\Services\DataTable;
 use Yajra\DataTables\EloquentDataTable;
 
-class EnvironnementDataTable extends DataTable
+class EnvironnementDataTable extends AbstractCommonDatatable
 {
     /**
      * Build DataTable class.
@@ -30,31 +30,6 @@ class EnvironnementDataTable extends DataTable
     public function query(Environnement $model)
     {
         return $model->newQuery();
-    }
-
-    /**
-     * Optional method if you want to use html builder.
-     *
-     * @return \Yajra\DataTables\Html\Builder
-     */
-    public function html()
-    {
-        return $this->builder()
-            ->columns($this->getColumns())
-            ->minifiedAjax()
-            ->addAction(['width' => '120px', 'printable' => false])
-            ->parameters([
-                'dom'       => 'Bfrtip',
-                'stateSave' => true,
-                'order'     => [[0, 'desc']],
-                'buttons'   => [
-                    ['extend' => 'create', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'export', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'print', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reset', 'className' => 'btn btn-default btn-sm no-corner',],
-                    ['extend' => 'reload', 'className' => 'btn btn-default btn-sm no-corner',],
-                ],
-            ]);
     }
 
     /**
