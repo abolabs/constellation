cf198ae8d0307f270343d053c26cfb3a7e6a217e
Alexandre Bordin <bordin.alexandre@gmail.com>
2021-11-11 14:40:01 +0100

    feat(login): ajout style principale sur login
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>{{config('app.name')}}</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
-    @yield('additional_headers')
+    @include('layouts.head')
 </head>
 <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
 <header class="app-header navbar">
