ad37cbb644bf8a26e3913c8c013f9a705ec64af2
Alexandre Bordin <bordin.alexandre@gmail.com>
2021-12-22 22:24:07 +0100

    feat(localisation): initialisation à partir de la langue du navigateur
    + correction mauvaise redirection après login
    
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -7,6 +7,8 @@ use Closure;
 
 class Localization
 {
+    const SESSION_KEY = 'locale';
+    const LOCALES = ['fr', 'en'];
     /**
      * Handle an incoming request.
      *
@@ -16,9 +18,21 @@ class Localization
      */
     public function handle($request, Closure $next)
     {
-        if (session()->has('locale')) {
-            App::setLocale(session()->get('locale'));
+        /** @var Session $session */
+        $session = $request->getSession();
+
+        if (!$session->has(self::SESSION_KEY)) {
+        $session->put(self::SESSION_KEY, $request->getPreferredLanguage(self::LOCALES));
         }
+
+        if ($request->has('lang')) {
+        $lang = $request->get('lang');
+        if (in_array($lang, self::LOCALES)) {
+            $session->put(self::SESSION_KEY, $lang);
+        }
+        }
+
+        app()->setLocale($session->get(self::SESSION_KEY));
 
         return $next($request);
     }
