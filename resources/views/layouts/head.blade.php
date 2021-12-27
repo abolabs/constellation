6d999073c8fbb26eba2d92c0935d495ca3de338a
Alexandre Bordin <bordin.alexandre@gmail.com>
2021-12-27 18:34:52 +0000

    fix(docker): build - correction import lib lang

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,6 +3,5 @@
 <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
 <link rel="stylesheet" href="/css/app.css">
-<script src="{{ asset('/js/Lang.js') }}"></script>
 <script src="/js/app.js"></script>
 @yield('additional_headers')
