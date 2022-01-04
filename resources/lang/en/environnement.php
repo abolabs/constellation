49b0d3edb8e43db20a69e5f9e5a5b87ad554f5fa
Alexandre Bordin <bordin.alexandre@gmail.com>
2022-01-04 18:45:31 +0100

    fix(sonar): Environnement API - usage fichier de langue
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- resources/lang/en/environnement.php
+++ resources/lang/en/environnement.php
@@ -6,4 +6,9 @@ return [
     'id' => 'ID',
     'created_at' => 'Created at',
     'update_at' => 'Updated at',
+    'not_found' => 'Environment not found',
+    'delete_confirm' => 'Environment deleted successfully',
+    'update_confirm' => 'Environment updated successfully',
+    'show_confirm' => 'Environment retrieved successfully',
+    'saved_confirm' => 'Environment saved successfully',
 ];
