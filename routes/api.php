dd294024281434004670fe5c7e5ac0b729f08226
Alexandre Bordin <bordin.alexandre@gmail.com>
2022-01-04 20:10:47 +0100

    fix(route): correction nom de route
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 // API V1
 Route::group(['middleware' => 'api', 'prefix' => 'v1/'], function () {
-    Route::resource('environnements', EnvironnementAPIController::class, ['names' => 'v1']);
-    Route::resource('hostingTypes', HostingTypeAPIController::class, ['names' => 'v1']);
-    Route::resource('hostings', HostingAPIController::class, ['names' => 'v1']);
-    Route::resource('teams', TeamAPIController::class, ['names' => 'v1']);
-    Route::resource('applications', ApplicationAPIController::class, ['names' => 'v1']);
-    Route::resource('services', ServiceAPIController::class, ['names' => 'v1']);
-    Route::resource('serviceVersions', ServiceVersionAPIController::class, ['names' => 'v1']);
-    Route::resource('service_version_dependencies', ServiceVersionDependenciesAPIController::class, ['names' => 'v1']);
-    Route::resource('service_instances', ServiceInstanceAPIController::class, ['names' => 'v1']);
-    Route::resource('serviceInstanceDependencies', ServiceInstanceDependenciesAPIController::class, ['names' => 'v1']);
+    Route::resource('environnements', EnvironnementAPIController::class, ['names' => 'v1.environnements']);
+    Route::resource('hostingTypes', HostingTypeAPIController::class, ['names' => 'v1.hostingTypes']);
+    Route::resource('hostings', HostingAPIController::class, ['names' => 'v1.hostings']);
+    Route::resource('teams', TeamAPIController::class, ['names' => 'v1.teams']);
+    Route::resource('applications', ApplicationAPIController::class, ['names' => 'v1.applications']);
+    Route::resource('services', ServiceAPIController::class, ['names' => 'v1.services']);
+    Route::resource('serviceVersions', ServiceVersionAPIController::class, ['names' => 'v1.serviceVersions']);
+    Route::resource('service_version_dependencies', ServiceVersionDependenciesAPIController::class, ['names' => 'v1.service_version_dependencies']);
+    Route::resource('service_instances', ServiceInstanceAPIController::class, ['names' => 'v1.service_instances']);
+    Route::resource('serviceInstanceDependencies', ServiceInstanceDependenciesAPIController::class, ['names' => 'v1.serviceInstanceDependencies']);
 });
