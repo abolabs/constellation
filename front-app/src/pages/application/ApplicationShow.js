67104c0abc1dbbf1f77282f01d183f225eb9fb03
Alexandre Bordin <bordin.alexandre@gmail.com>
2022-12-28 17:55:14 +0100

    refactor(front): extract instance card into a separated component !71
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- front-app/src/pages/application/ApplicationShow.js
+++ front-app/src/pages/application/ApplicationShow.js
@@ -1,17 +1,15 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
   Card,
-  CardActions,
   CardHeader,
   CardContent,
   Chip,
   Grid,
   Fade,
   LinearProgress,
-  Link,
   List,
   ListItem,
   ListItemButton,
@@ -33,11 +31,11 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import EditIcon from "@mui/icons-material/Edit";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import { grey } from "@mui/material/colors";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 import AppBreadCrumd from "@layouts/AppBreadCrumd";
 import AlertError from "@components/alerts/AlertError";
 import CreateServiceInstanceModal from "@pages/serviceInstance/CreateServiceInstanceModal";
+import ServiceInstanceCard from "../serviceInstance/ServiceInstanceCard";
 
 const ApplicationShow = () => {
   const location = useLocation();
@@ -126,7 +124,7 @@ const ApplicationShow = () => {
                       ? (
                         <Fade key={instance?.id} in={true} timeout={500}>
                           <Grid item xs={8} sm={8} md={4} lg={3}>
-                              <InstanceCard key={instance?.id} {...instance} {...currentEnvId}/>
+                              <ServiceInstanceCard key={instance?.id} {...instance} {...currentEnvId}/>
                           </Grid>
                         </Fade>
                       ) :null
@@ -145,87 +143,6 @@ const ApplicationShow = () => {
         handleClose={() => setOpenModal(false)}
       />
     </>
-  );
-};
-
-const InstanceCard = (instance) => {
-  const navigate = useNavigate();
-  const theme = useTheme();
-
-  const onClick= useCallback(() => {
-    navigate(`/service_instances/${instance?.id}/show`);
-  }, [instance?.id]);
-
-  return (
-    <Card sx={{
-      height: '26vh',
-    }}>
-      <CardHeader
-        title={instance?.service_version_name}
-        sx={{
-          background: theme?.palette?.secondary?.main,
-          color: theme?.palette?.secondary?.contrastText,
-        }}
-        action={
-          <Chip
-            label={`Version ${instance?.service_version}`}
-            color="primary"
-            size="small"
-          />
-        }
-      />
-      <CardContent sx={{
-        height: "60%",
-        "& .MuiTypography-body1": {
-          fontWeight: "bold",
-        }
-      }}>
-        <List
-          sx={{
-            "& .MuiListItem-root": {
-              padding: 0,
-            },
-          }}
-        >
-          <ListItem sx={{flexWrap: "wrap"}}>
-            <Chip label={`ID: ${instance?.id}`} color="primary" size="small" />
-            &nbsp;
-            <Chip
-              label={`Statut: ${instance?.statut ? 'Active' : 'Inactive'}`}
-              color={instance?.statut ? 'success' : 'warning'}
-              size="small"
-            />
-            &nbsp;
-            {instance?.role ? <Chip label={`Role: ${instance?.role}`} color="secondary" size="small" /> : null}
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary="Hébergement"
-              secondary={instance?.hosting_name}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemText
-              primary="Dépôt Git"
-              secondary={
-                <Link href={instance?.url}>
-                  {instance?.url}
-                </Link>
-              }
-            />
-          </ListItem>
-        </List>
-      </CardContent>
-      <CardActions style={{justifyContent: 'center'}}>
-        <Button
-          variant="outlined"
-          endIcon={<KeyboardArrowRightIcon />}
-          onClick={onClick}
-        >
-          Voir plus
-        </Button>
-      </CardActions>
-    </Card>
   );
 };
 
