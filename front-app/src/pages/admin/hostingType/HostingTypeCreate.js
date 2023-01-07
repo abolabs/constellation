f5815f304559953dcdca9a0345f87afaf882c31f
Alexandre Bordin <bordin.alexandre@gmail.com>
2023-01-07 19:18:43 +0100

    feat(front): Migrate Hosting Type creation page #56
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

---
+++ front-app/src/pages/admin/hostingType/HostingTypeCreate.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useLocation } from 'react-router-dom';
+import Typography from "@mui/material/Typography";
+
+import AppBreadCrumd from "@layouts/AppBreadCrumd";
+import DefaultEditToolBar from '@/components/toolbar/DefaultEditToolBar';
+import HostingTypeDefaultSchema from './HostingTypeDefaultSchema';
+
+const HostingTypeCreate = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <AppBreadCrumd location={location} />
+      <Typography variant="h3">Hosting Type</Typography>
+      <Create>
+        <SimpleForm
+          resolver={yupResolver(HostingTypeDefaultSchema)}
+          toolbar={<DefaultEditToolBar />}
+        >
+          <TextInput source="name" fullWidth />
+          <TextInput source="description" fullWidth />
+        </SimpleForm>
+      </Create>
+    </>
+  );
+};
+
+export default HostingTypeCreate;
