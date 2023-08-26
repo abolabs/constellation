22bfd7edc0f2642eaca417cf6008c9f68f323be2
Alexandre Bordin <bordin.alexandre@gmail.com>
2023-08-26 16:38:02 +0200

    improve(web-ui): replace toggle sidebar button with Constellation logo
    Signed-off-by: Alexandre Bordin <bordin.alexandre@gmail.com>

--- web-ui/src/layouts/DefaultAppBar.jsx
+++ web-ui/src/layouts/DefaultAppBar.jsx
@@ -15,10 +15,24 @@
 
 // external-lib
 import * as React from "react";
-import { Logout, UserMenu, AppBar, useTranslate } from "react-admin";
+import {
+  AppBarClasses,
+  HideOnScroll,
+  LoadingIndicator,
+  LocalesMenuButton,
+  Logout,
+  TitlePortal,
+  UserMenu,
+  useLocales,
+  useSidebarState,
+  useThemesContext,
+  useTranslate,
+} from "react-admin";
 import { Link } from "react-router-dom";
 
-import { useTheme } from "@mui/material/styles";
+import { AppBar as MuiAppBar, Toolbar } from "@mui/material";
+
+import { styled, useTheme } from "@mui/material/styles";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -29,7 +43,7 @@ import Brightness7Icon from "@mui/icons-material/Brightness7";
 import IconButton from "@mui/material/IconButton";
 // local
 import ColorModeContext from "@contexts/ColorModeContext";
-import { Avatar, SvgIcon } from "@mui/material";
+import { Avatar, SvgIcon, useMediaQuery } from "@mui/material";
 import { ReactComponent as LogoSvg } from "@/logo50.svg";
 
 // It's important to pass the ref to allow MUI to manage the keyboard navigation
@@ -77,6 +91,124 @@ const ToggleThemeButton = (props) => {
   );
 };
 
+const DefaultToolbar = () => {
+  const locales = useLocales();
+  const { darkTheme } = useThemesContext();
+  return (
+    <>
+      {locales && locales.length > 1 ? <LocalesMenuButton /> : null}
+      {darkTheme && <ToggleThemeButton />}
+      <LoadingIndicator />
+    </>
+  );
+};
+
+const defaultToolbarElement = <DefaultToolbar />;
+
+const DefaultUserMenu = <UserMenu />;
+
+const PREFIX = "RaAppBar";
+
+const StyledAppBar = styled(MuiAppBar, {
+  name: PREFIX,
+  overridesResolver: (props, styles) => styles.root,
+})(({ theme }) => ({
+  [`& .${AppBarClasses.toolbar}`]: {
+    padding: `0 ${theme.spacing(1)}`,
+    [theme.breakpoints.down("md")]: {
+      minHeight: theme.spacing(6),
+    },
+  },
+  [`& .${AppBarClasses.menuButton}`]: {
+    marginRight: "0.2em",
+  },
+  [`& .${AppBarClasses.title}`]: {},
+}));
+
+const AppBar = React.memo((props) => {
+  const {
+    alwaysOn,
+    children,
+    className,
+    color = "secondary",
+    open,
+    title,
+    toolbar = defaultToolbarElement,
+    userMenu = DefaultUserMenu,
+    container: Container = alwaysOn ? "div" : HideOnScroll,
+    ...rest
+  } = props;
+
+  const isXSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const theme = useTheme();
+  // eslint-disable-next-line no-unused-vars
+  const [_openSideBar, setOpenSideBar] = useSidebarState();
+
+  console.log(theme.transitions);
+
+  return (
+    <Container className={className}>
+      <StyledAppBar className={AppBarClasses.appBar} color={color} {...rest}>
+        <Toolbar
+          disableGutters
+          variant={isXSmall ? "regular" : "dense"}
+          className={AppBarClasses.toolbar}
+        >
+          <Avatar
+            onClick={() => setOpenSideBar(!open)}
+            sx={{
+              m: 1,
+              bgcolor: "primary.main",
+              height: "2rem",
+              width: "2rem",
+              p: 0,
+              margin: "0.5rem 0.5rem 0.5rem 0rem",
+              display: "inline-flex",
+              "&:hover": {
+                cursor: "pointer",
+              },
+              "&:active": {
+                transition: theme.transitions.create(["transform"], {
+                  easing: theme.transitions.easing.sharp,
+                  duration: theme.transitions.duration.complex,
+                }),
+                transform: "rotate(-1turn)",
+              },
+            }}
+          >
+            <SvgIcon
+              component={LogoSvg}
+              inheritViewBox
+              shapeRendering="path"
+              color="primary"
+              sx={{
+                path: {
+                  fill: `${theme.palette.primary.contrastText} !important`,
+                },
+                height: "80%",
+                width: "80%",
+              }}
+            />
+          </Avatar>
+          {React.Children.count(children) === 0 ? (
+            <TitlePortal className={AppBarClasses.title} />
+          ) : (
+            children
+          )}
+          {toolbar}
+          {typeof userMenu === "boolean" ? (
+            userMenu === true ? (
+              <UserMenu />
+            ) : null
+          ) : (
+            userMenu
+          )}
+        </Toolbar>
+      </StyledAppBar>
+    </Container>
+  );
+});
+
 const DefaultAppBar = (props) => {
   const theme = useTheme();
   return (
@@ -97,31 +229,6 @@ const DefaultAppBar = (props) => {
         component="div"
         sx={{ flexGrow: 1, display: "flex", alignContent: "center" }}
       >
-        <Avatar
-          sx={{
-            m: 1,
-            bgcolor: "primary.main",
-            height: "1.25rem",
-            width: "1.25rem",
-            p: 0,
-            margin: "0.25rem",
-            display: "inline-flex",
-          }}
-        >
-          <SvgIcon
-            component={LogoSvg}
-            inheritViewBox
-            shapeRendering="path"
-            color="primary"
-            sx={{
-              path: {
-                fill: `${theme.palette.primary.contrastText} !important`,
-              },
-              height: "80%",
-              width: "80%",
-            }}
-          />
-        </Avatar>
         Constellation
       </Typography>
       <ToggleThemeButton />
